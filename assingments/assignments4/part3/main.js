c8f353e06377f98c3b914151c3d31f510c4f348c
zZaidSs <zaidtimes0@gmail.com>
2024-08-09 19:31:20 -0400

    Update main.js
    added collision detection

--- assingments/assignments4/part3/main.js
+++ assingments/assignments4/part3/main.js
@@ -45,3 +45,18 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          this.color = ball.color = randomRGB();
+        }
+      }
+    }
+  }
+}
