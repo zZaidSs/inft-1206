57398f3c410cb11cca21f4c67475e9acf5f4d2dc
zZaidSs <zaidtimes0@gmail.com>
2024-08-09 20:14:56 -0400

    finished part4
    fixed html error and minor bugs also added score counter to count balls

--- assingments/assignments4/part4/main-finished.js
+++ assingments/assignments4/part4/main-finished.js
@@ -1,5 +1,4 @@
 // set up canvas
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,16 +6,16 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function to generate random RGB color value
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// Shape class
 class Shape {
   constructor(x, y, velX, velY) {
     this.x = x;
@@ -47,12 +46,15 @@ class Ball extends Shape {
     if (this.x + this.size >= width) {
       this.velX = -Math.abs(this.velX);
     }
+
     if (this.x - this.size <= 0) {
       this.velX = Math.abs(this.velX);
     }
+
     if (this.y + this.size >= height) {
       this.velY = -Math.abs(this.velY);
     }
+
     if (this.y - this.size <= 0) {
       this.velY = Math.abs(this.velY);
     }
@@ -76,37 +78,6 @@ class Ball extends Shape {
   }
 }
 
-
-const balls = [];
-
-while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size
-  );
-
-  balls.push(ball);
-}
-
-function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-  ctx.fillRect(0, 0, width, height);
-
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
-
-  requestAnimationFrame(loop);
-}
 class EvilCircle extends Shape {
   constructor(x, y) {
     super(x, y, 20, 20);
@@ -170,7 +141,25 @@ class EvilCircle extends Shape {
     }
   }
 }
+
+const balls = [];
 const evilCircle = new EvilCircle(300, 300);
+let ballCount = 0;
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    size,
+    randomRGB()
+  );
+
+  balls.push(ball);
+  ballCount++;
+}
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
@@ -191,6 +180,9 @@ function loop() {
   requestAnimationFrame(loop);
 }
 
+function updateBallCount() {
+  document.getElementById('ballCount').textContent = `Ball count: ${ballCount}`;
+}
+
+updateBallCount();
 loop();
-
-
